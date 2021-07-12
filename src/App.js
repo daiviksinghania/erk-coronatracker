d4bf1216c833fb974f728d601978d335b1d79d11
errahulkumar <er.rahulkumar13@gmail.com>
2021-07-12 23:20:40 +0530

    corona tracker

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import StateWise from './components/StateWiseData/StateWise';
+
+function App() {
+  return (
+    <StateWise></StateWise>
+  );
+}
+
+export default App;
---
+++ src/components/StateWiseData/StateWise.js
@@ -0,0 +1,58 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import './statewise.css'
+
+const StateWise = () => {
+    const [data, setData] = useState([]);
+    const getCovidData = async () => {
+        const response = await axios.get('https://api.covid19india.org/data.json');
+       setData(response.data.statewise);
+
+    }
+
+    useEffect(() => {
+        getCovidData();
+    }, []);
+
+    return (
+        <div className="container-fluid mt-5">
+            <div className="main-heading">
+                <h1 className="mb-5 text-center">Corona Tracker <span className="font-weight-bold">India</span></h1>
+            </div>
+            <div className="table-responsive">
+                <table className="table table-hover">
+                    <thead className="thead-dark">
+                        <tr>
+                            <th>State</th>
+                            <th>Confirmed</th>
+                            <th>Recovered</th>
+                            <th>Deaths</th>
+                            <th>Active</th>
+                            <th>Updated</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            data.map((elem, index)=>{
+                                return(
+                                    <tr key={index}>
+                                        <td>{elem.state}</td>
+                                        <td>{elem.confirmed}</td>
+                                        <td>{elem.recovered}</td>
+                                        <td>{elem.deaths}</td>
+                                        <td>{elem.active}</td>
+                                        <td>{elem.lastupdatedtime}</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                        
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    )
+}
+
+export default StateWise
